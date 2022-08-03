e0378a4611f7bd9c1966e5252352ee8eb6db6039
Paul Crow <paucrow@synamedia.com>
2022-08-03 08:40:08 +0100

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by paucrow on 03/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

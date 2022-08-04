49706f232b770a66e2dab1410333865b840c0289
Paul Crow <paucrow@synamedia.com>
2022-08-04 15:20:32 +0100

    WIP

---
+++ Drawing/ImagePaint.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePaint.swift
+//  Drawing
+//
+//  Created by paucrow on 03/08/2022.
+//
+
+import SwiftUI
+
+//struct ContentView: View {
+//
+//    var body: some View {
+//            Text("Offset")
+//            .frame(width: 300, height: 300)
+//            .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.4, width: 1, height: 0.2), scale: 0.1), width: 30)
+//
+//    }
+//}
